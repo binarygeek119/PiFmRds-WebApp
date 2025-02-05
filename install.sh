1a648f9049ea6d6b7918ea7c59faa3c4c2115b61
furio <139717584+furipaf@users.noreply.github.com>
2025-02-05 12:37:03 +0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -18,6 +18,17 @@ cd PiFmRds/src
 make clean
 make
 
+echo "================================="
 echo "Installation complete!"
 echo "To start the web application, run:"
 echo "node server.js"
+echo "================================="
+echo "                         "
+echo "   __            _       "
+echo "  / _|          (_)      "
+echo " | |_ _   _ _ __ _  ___  "
+echo " |  _| | | | '__| |/ _ \ "
+echo " | | | |_| | |  | | (_) |"
+echo " |_|  \__,_|_|  |_|\___/ "
+echo "                         "
+echo "                         "
